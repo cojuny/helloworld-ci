9fe2c2114f81de9ca403c255fd8660356d687062
Junyoung Wang <codingdaniel0205@outlook.com>
2023-09-03 17:05:19 +0800

    init

---
+++ src/main/java/com/cojuny/App.java
@@ -0,0 +1,13 @@
+package com.cojuny;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
